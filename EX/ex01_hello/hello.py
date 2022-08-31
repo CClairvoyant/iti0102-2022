a332c8c44eb8ba67ea583c32a03fcc97da93c2e3
anviks <anviks@ttu.ee>
2022-08-31 22:16:29 +0300

    Hello!

---
+++ EX/ex01_hello/hello.py
@@ -0,0 +1,6 @@
+name = input("What is your name? ")
+num1 = int(input(f"Hello {name}! Enter a random number: "))
+num2 = int(input(f"Great! Now enter a second random number: "))
+print(f"{num1} + {num2} is {num1+num2}")
+
+
