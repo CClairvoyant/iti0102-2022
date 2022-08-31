13432ae01312e8eeb276453ac93e4e876d067d68
anviks <anviks@ttu.ee>
2022-09-01 00:30:46 +0300

    Cashier

--- EX/ex01_hello/poem.py
+++ EX/ex01_hello/poem.py
@@ -1,4 +1,4 @@
 colour = input("Hello! To begin with creating your poem, enter a colour: ")
 items = input("Nice! Now enter a plural noun: ")
 activity = input("Now lastly, enter an activity (to ...): ")
-print(f"Roses are {colour}, \n{items} are blue, \nI love to {activity} \nand so will you!")
+print(f"Roses are {colour}, \n{items} are blue, \nI love to {activity} \nAnd so will you!")
