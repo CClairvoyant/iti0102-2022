bbe40a9d59ba6e83a16c9d990d859eaf3c1aedda
anviks <anviks@ttu.ee>
2022-09-30 14:59:22 +0300

    Hobbies task

--- EX/ex05_hobbies/hobbies.py
+++ EX/ex05_hobbies/hobbies.py
@@ -62,13 +62,38 @@ def create_dictionary_with_hobbies(data: str) -> dict:
     return people_hobbies
 
 
-if __name__ == '__main__':
-    sample_data = """Jack:crafting\nPeter:hiking\nWendy:gaming\nMonica:tennis\nChris:origami\nSophie:sport\nMonica:design\nCarmen:sport\nChris:sport\nMonica:skateboarding\nCarmen:cooking\nWendy:photography\nMonica:tennis\nCooper:yoga\nWendy:sport\nCooper:movies\nMonica:theatre\nCooper:yoga\nChris:gaming\nMolly:fishing\nJack:skateboarding\nWendy:fishing\nJack:drawing\nMonica:baking\nSophie:baking\nAlfred:driving\nAlfred:shopping\nAlfred:crafting\nJack:drawing\nCarmen:shopping\nCarmen:driving\nPeter:drawing\nCarmen:shopping\nWendy:fitness\nAlfred:travel\nJack:origami\nSophie:design\nJack:pets\nCarmen:dance\nAlfred:baking\nSophie:sport\nPeter:gaming\nJack:skateboarding\nCooper:football\nAlfred:sport\nCooper:fitness\nChris:yoga\nWendy:football\nMolly:design\nJack:hiking\nMonica:pets\nCarmen:photography\nJack:baking\nPeter:driving\nChris:driving\nCarmen:driving\nPeter:theatre\nMolly:hiking\nWendy:puzzles\nJack:crafting\nPeter:photography\nCarmen:theatre\nSophie:crafting\nCarmen:cooking\nAlfred:gaming\nPeter:theatre\nCooper:hiking\nChris:football\nChris:pets\nJack:football\nMonica:skateboarding\nChris:driving\nCarmen:pets\nCooper:gaming\nChris:hiking\nJack:cooking\nPeter:fishing\nJack:gaming\nPeter:origami\nCarmen:movies\nSophie:driving\nJack:sport\nCarmen:theatre\nWendy:shopping\nCarmen:pets\nWendy:gaming\nSophie:football\nWendy:theatre\nCarmen:football\nMolly:theatre\nPeter:theatre\nMonica:flowers\nMolly:skateboarding\nPeter:driving\nSophie:travel\nMonica:photography\nCooper:cooking\nJack:fitness\nPeter:cooking\nChris:gaming"""
-    dic = create_dictionary_with_hobbies(sample_data)
-    # print("shopping" in dic["Wendy"])  # -> True
-    # print("fitness" in dic["Sophie"])  # -> False
-    # print("gaming" in dic["Peter"])  # -> True
-    # print(len(dic["Jack"]))  # ->  12
-    # print(len(dic["Carmen"]))  # -> 10
-    # print(len(dic["Molly"]))  # -> 5
-    # print(len(dic["Sophie"]))  # -> 7
+def find_people_with_most_hobbies(data: str) -> list:
+    """
+    Find the people who have the most hobbies.
+    :param data: given string from database
+    :return: list of people with most hobbies. Sorted alphabetically.
+    """
+    people_with_hobbies = create_dictionary(data)
+    counts_of_hobbies = []
+    people_with_most_hobbies = []
+    for person in people_with_hobbies:
+        counts_of_hobbies.append(len(people_with_hobbies.get(person)))
+    max_count_of_hobbies = max(counts_of_hobbies)
+    for person in people_with_hobbies:
+        if len(people_with_hobbies.get(person)) == max_count_of_hobbies:
+            people_with_most_hobbies.append(person)
+    return sorted(people_with_most_hobbies)
+
+
+def find_least_popular_hobbies(data: str) -> list:
+    """
+    Find the least popular hobbies.
+
+    :param data: given string from database
+    :return: list of least popular hobbies. Sorted alphabetically.
+    """
+    hobby_list = create_dictionary_with_hobbies(data)
+    counts_of_people = []
+    hobbies_with_least_people = []
+    for hobby in hobby_list:
+        counts_of_people.append(len(hobby_list.get(hobby)))
+    min_count_of_people = min(counts_of_people)
+    for hobby in hobby_list:
+        if len(hobby_list.get(hobby)) == min_count_of_people:
+            hobbies_with_least_people.append(hobby)
+    return sorted(hobbies_with_least_people)
