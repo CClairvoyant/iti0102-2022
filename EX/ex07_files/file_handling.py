ffe6e91ada9bbff947738b7990b1cadb114b32d1
anviks <anviks@ttu.ee>
2022-10-11 23:06:42 +0300

    File handling task

--- EX/ex07_files/file_handling.py
+++ EX/ex07_files/file_handling.py
@@ -1,6 +1,10 @@
+"""Operations with files."""
+
+
 def read_file_contents(filename: str) -> str:
     """
     Read file contents into string.
+
     In this exercise, we can assume the file exists.
 
     :param filename: File to read.
@@ -13,7 +17,7 @@ def read_file_contents(filename: str) -> str:
 
 
 def read_file_contents_to_list(filename: str) -> list:
-    """
+    r"""
     Read file contents into list of lines.
 
     In this exercise, we can assume the file exists.
