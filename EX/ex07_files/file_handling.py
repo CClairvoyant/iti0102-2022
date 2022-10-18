b0503a625b652e3ec42bf9b44639d0c2cfeae5f2
anviks <anviks@ttu.ee>
2022-10-18 13:36:55 +0300

    File handling task

--- EX/ex07_files/file_handling.py
+++ EX/ex07_files/file_handling.py
@@ -463,12 +463,11 @@ def read_people_data(directory: str) -> dict:
     files_list = os.listdir(directory)
     dodo = {}
     for file_name in files_list:
-        if file_name[-4:] == ".csv":
-            for dictionary in read_csv_file_into_list_of_dicts_using_datatypes(fr"{directory}/{file_name}"):
-                if dictionary["id"] not in dodo:
-                    dodo[dictionary["id"]] = dictionary
-                else:
-                    dodo[dictionary["id"]].update(dictionary)
+        for dictionary in read_csv_file_into_list_of_dicts_using_datatypes(fr"{directory}/{file_name}"):
+            if dictionary["id"] not in dodo:
+                dodo[dictionary["id"]] = dictionary
+            else:
+                dodo[dictionary["id"]].update(dictionary)
     datatype_list = []
     for key in dodo.keys():
         for gogo in dodo[key]:
