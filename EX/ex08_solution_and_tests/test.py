e9698de6cf7a287c5efab9c2d0632655b6e84ad9
anviks <anviks@ttu.ee>
2022-10-20 15:55:38 +0300

    File handling task

--- EX/ex08_solution_and_tests/test.py
+++ EX/ex08_solution_and_tests/test.py
@@ -3,9 +3,12 @@ import solution
 
 
 def test_solve_test():
-    assert solution.students_study(19, False) is True
+    assert solution.students_study(18, False) is True
     assert solution.students_study(1, True) is False
-    assert solution.students_study(20, True) is True
-    assert solution.students_study(16, False) is False
-    assert solution.students_study(15, True) is True
-    assert solution.students_study(2, False) is False
+    assert solution.students_study(18, True) is True
+    assert solution.students_study(17, False) is False
+    assert solution.students_study(5, True) is True
+    assert solution.students_study(4, False) is False
+    assert solution.students_study(18, False) is True
+    assert solution.students_study(1, False) is False
+    assert solution.students_study(4, True) is False
