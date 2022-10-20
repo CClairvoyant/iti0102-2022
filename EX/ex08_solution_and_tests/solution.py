a4945b197b0dbc1f1a09c3881aa5d4110a92e031
anviks <anviks@ttu.ee>
2022-10-20 10:21:20 +0300

    File handling task

---
+++ EX/ex08_solution_and_tests/solution.py
@@ -0,0 +1,43 @@
+def students_study(time: int, coffee_needed: bool) -> bool:
+    """
+    Return True if students study in given circumstances.
+
+    (19, False) -> True
+    (1, True) -> False.
+    """
+    return 18 <= time <= 24 or 5 <= time < 18 and coffee_needed
+
+
+def lottery(a: int, b: int, c: int) -> int:
+    """
+    Return Lottery victory result 10, 5, 1, or 0 according to input values.
+
+    (5, 5, 5) -> 10
+    (2, 2, 1) -> 0
+    (2, 3, 1) -> 1
+    """
+    if a == b == c == 5:
+        return 10
+    elif a == b == c:
+        return 5
+    elif b != a != c:
+        return 1
+    else:
+        return 0
+
+
+def fruit_order(small_baskets: int, big_baskets: int, ordered_amount: int) -> int:
+    """
+    Return number of small fruit baskets if it's possible to finish the order, otherwise return -1.
+
+    (4, 1, 9) -> 4
+    (3, 1, 10) -> -1
+    """
+    if ordered_amount == big_baskets * 5 + small_baskets:
+        return small_baskets
+    else:
+        return -1
+
+
+if __name__ == '__main__':
+    print(students_study(1, True))
