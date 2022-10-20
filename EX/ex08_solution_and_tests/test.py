eb5b25faa74fc9e82731ae4a8d95095643876aa4
anviks <anviks@ttu.ee>
2022-10-20 18:31:13 +0300

    Testing my written functions.

--- EX/ex08_solution_and_tests/test.py
+++ EX/ex08_solution_and_tests/test.py
@@ -1,11 +1,11 @@
 """Tests of solution.py."""
 
 
-import pytest
 import solution
 
 
 def test_students_study_night():
+    """Test function with nighttime's first and last hours."""
     assert solution.students_study(1, True) is False
     assert solution.students_study(1, False) is False
     assert solution.students_study(4, True) is False
@@ -13,6 +13,7 @@ def test_students_study_night():
 
 
 def test_students_study_day():
+    """Test function with daytime's first and last hours."""
     assert solution.students_study(5, True) is True
     assert solution.students_study(5, False) is False
     assert solution.students_study(17, True) is True
@@ -20,6 +21,7 @@ def test_students_study_day():
 
 
 def test_students_study_evening():
+    """Test function with evening's first and last hours."""
     assert solution.students_study(18, True) is True
     assert solution.students_study(18, False) is True
     assert solution.students_study(24, True) is True
@@ -27,10 +29,12 @@ def test_students_study_evening():
 
 
 def test_lottery_all_fives():
+    """Test function with jackpot numbers."""
     assert solution.lottery(5, 5, 5) == 10
 
 
 def test_lottery_same_numbers():
+    """Test function with a, b, c being the same, but not 5."""
     assert solution.lottery(3, 3, 3) == 5
     assert solution.lottery(17, 17, 17) == 5
     assert solution.lottery(-6, -6, -6) == 5
@@ -38,13 +42,19 @@ def test_lottery_same_numbers():
 
 
 def test_lottery_two_numbers_same():
+    """Test function with two numbers being the same and one being different."""
     assert solution.lottery(5, 5, 4) == 0
     assert solution.lottery(1, 10, 1) == 0
     assert solution.lottery(6, 8, 8) == 1
+
+
+def test_lottery_all_different():
+    """Test function with all numbers being different."""
     assert solution.lottery(6, 3, 23) == 1
 
 
 def test_fruit_basket_zero():
+    """Test function with ordered amount being zero."""
     assert solution.fruit_order(0, 0, 0) == 0
     assert solution.fruit_order(0, 4, 0) == 0
     assert solution.fruit_order(3, 0, 0) == 0
@@ -52,6 +62,7 @@ def test_fruit_basket_zero():
 
 
 def test_fruit_basket_only_large():
+    """Test function with no small baskets."""
     assert solution.fruit_order(0, 3, 15) == 0
     assert solution.fruit_order(0, 1, 10) == -1
     assert solution.fruit_order(0, 1, 12) == -1
@@ -61,6 +72,7 @@ def test_fruit_basket_only_large():
 
 
 def test_fruit_basket_only_small():
+    """Test function with no large baskets."""
     assert solution.fruit_order(8, 0, 6) == 6
     assert solution.fruit_order(9, 0, 11) == -1
     assert solution.fruit_order(4, 0, 4) == 4
@@ -69,6 +81,7 @@ def test_fruit_basket_only_small():
 
 
 def test_fruit_basket_small_and_large():
+    """Test function with various combinations of both baskets and ordered amount."""
     assert solution.fruit_order(3, 2, 13) == 3
     assert solution.fruit_order(2, 5, 12) == 2
     assert solution.fruit_order(8, 1, 16) == -1
