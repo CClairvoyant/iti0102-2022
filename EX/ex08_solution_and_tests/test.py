482dfa8ca32c91f754dfad1fe6bcc50c3b4f8d5a
anviks <anviks@ttu.ee>
2022-10-20 17:31:43 +0300

    File handling task

--- EX/ex08_solution_and_tests/test.py
+++ EX/ex08_solution_and_tests/test.py
@@ -1,3 +1,6 @@
+"""Tests of solution.py"""
+
+
 import pytest
 import solution
 
@@ -24,18 +27,44 @@ def test_students_study_evening():
 
 
 def test_lottery_all_fives():
-    assert solution.lottery(5, 5, 5) is 10
+    assert solution.lottery(5, 5, 5) == 10
 
 
 def test_lottery_same_numbers():
-    assert solution.lottery(3, 3, 3) is 5
-    assert solution.lottery(17, 17, 17) is 5
-    assert solution.lottery(-6, -6, -6) is 5
-    assert solution.lottery(0, 0, 0) is 5
+    assert solution.lottery(3, 3, 3) == 5
+    assert solution.lottery(17, 17, 17) == 5
+    assert solution.lottery(-6, -6, -6) == 5
+    assert solution.lottery(0, 0, 0) == 5
 
 
 def test_lottery_two_numbers_same():
-    assert solution.lottery(5, 5, 4) is 0
-    assert solution.lottery(1, 10, 1) is 0
-    assert solution.lottery(6, 8, 8) is 1
-    assert solution.lottery(6, 3, 23) is 1
+    assert solution.lottery(5, 5, 4) == 0
+    assert solution.lottery(1, 10, 1) == 0
+    assert solution.lottery(6, 8, 8) == 1
+    assert solution.lottery(6, 3, 23) == 1
+
+
+def test_fruit_basket_zero():
+    assert solution.fruit_order(0, 0, 0) == 0
+    assert solution.fruit_order(0, 4, 0) == 0
+    assert solution.fruit_order(3, 0, 0) == 0
+    assert solution.fruit_order(2, 4, 0) == 0
+
+
+def test_fruit_basket_large_exactly_enough():
+    assert solution.fruit_order(3, 3, 15) == 0
+
+
+def test_fruit_basket_large_not_enough():
+    assert solution.fruit_order(10, 1, 12) == 7
+    assert solution.fruit_order(2, 1, 12) == -1
+    assert solution.fruit_order(12, 1, 15) == 10
+    assert solution.fruit_order(2, 1, 15) == -1
+
+
+def test_fruit_basket_large_more_than_enough():
+    assert solution.fruit_order(3, 5, 7) == 2
+    assert solution.fruit_order(1, 5, 7) == -1
+
+
+
