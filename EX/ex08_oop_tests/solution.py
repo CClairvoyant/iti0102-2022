db809c8c45e5a4714b61cba657515d23b754a3fc
anviks <anviks@ttu.ee>
2022-10-21 22:23:27 +0300

    Some weird cake task.

--- EX/ex08_oop_tests/solution.py
+++ EX/ex08_oop_tests/solution.py
@@ -1,14 +1,17 @@
-from typing import List, Any
-
-import pytest
+"""Cake factory task."""
 
 
 class Factory:
 
+    """Cake factory class."""
+
     def __init__(self):
+        """Make a list of baked cakes."""
         self.cakes_baked = []
 
     def bake_cake(self, toppings: int, base: int) -> int:
+        """Bake large cakes if possible, then medium cakes if possible, then basic cakes, adds them to the list and
+        returns the amount of cakes baked."""
         amount = 0
         if toppings == base:
             amount = toppings // 5 + toppings % 5 // 2 + toppings % 5 % 2
@@ -18,15 +21,18 @@ class Factory:
         return amount
 
     def get_last_cakes(self, n: int) -> list:
+        """Return the last n baked cakes."""
         if n > 0:
             return self.cakes_baked[-n:]
         else:
             return []
 
     def get_cakes_baked(self) -> list:
+        """Return the baked cakes list."""
         return self.cakes_baked
 
     def __str__(self):
+        """Return a sentence with the amount of cakes baked."""
         if len(self.cakes_baked) == 1:
             return "Factory with 1 cake."
         else:
@@ -35,7 +41,10 @@ class Factory:
 
 class Cake:
 
+    """Class that is focused on assigning cake types."""
+
     def __init__(self, base_amount, toppings_amount):
+        """Check if the amounts of bases and toppings are 1, 2 or 5."""
         allowed_list = [1, 2, 5]
         if base_amount not in allowed_list and toppings_amount not in allowed_list:
             raise WrongIngredientsAmountException("Incorrect amount of ingredients!")
@@ -44,6 +53,7 @@ class Cake:
 
     @property
     def type(self):
+        """Find the type of cake."""
         if self.base_amount == 1 and self.toppings_amount == 1:
             return "basic"
         elif self.base_amount == 2 and self.toppings_amount == 2:
@@ -52,21 +62,24 @@ class Cake:
             return "large"
 
     def __repr__(self):
+        """Return the cake type."""
         return f"Cake({self.type})"
 
     def __eq__(self, other):
+        """Compare the types."""
         return self.type == other.type
 
 
 class WrongIngredientsAmountException(Exception):
+    """Exception triggered if the Cake class has been given the wrong amount of ingredients."""
     pass
 
 
 if __name__ == '__main__':
     factory = Factory()
-    factory.bake_cake(3, 3)
-    cakes = factory.get_last_cakes(2)
-    print(cakes)
-    print(cakes[0].type == "medium")
-    print(cakes[1].type != "medium")
-
+    print(factory)
+    print(factory.bake_cake(13, 13))
+    print(factory.bake_cake(2, 2))
+    print(factory.get_cakes_baked())
+    print(factory)
+    print(Cake(1, 1))
