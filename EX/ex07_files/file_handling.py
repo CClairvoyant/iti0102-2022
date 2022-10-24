38bdef2a2a44903b7b54e61abde3025d3d09a53d
anviks <anviks@ttu.ee>
2022-10-24 18:13:18 +0300

    Recursion is recursion.

--- EX/ex07_files/file_handling.py
+++ EX/ex07_files/file_handling.py
@@ -539,16 +539,12 @@ def generate_people_report(person_data_directory: str, report_filename: str) -> 
                     data_dict[id_num]["status"] = "alive"
                 else:
                     data_dict[id_num]["status"] = "dead"
-            if "age" not in data_dict[id_num]:
-                if data_dict[id_num]["birth"] is None:
-                    data_dict[id_num]["age"] = -1
-                elif data_dict[id_num]["death"] is None:
-                    data_dict[id_num]["age"] = calculate_age(data_dict[id_num]["birth"])
-                else:
-                    data_dict[id_num]["age"] = calculate_age(data_dict[id_num]["birth"], data_dict[id_num]["death"])
-            if data_dict[id_num]["age"] is None:
+            if data_dict[id_num]["birth"] is None:
                 data_dict[id_num]["age"] = -1
-        print(data_dict)
+            elif data_dict[id_num]["death"] is None:
+                data_dict[id_num]["age"] = calculate_age(data_dict[id_num]["birth"])
+            else:
+                data_dict[id_num]["age"] = calculate_age(data_dict[id_num]["birth"], data_dict[id_num]["death"])
         for id_num in data_dict:
             for key in list(data_dict[id_num].keys()):
                 report_list[0].append(key)
