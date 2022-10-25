1c4dd5a2e9145375290fa66e781f5922caeb0dd2
anviks <anviks@ttu.ee>
2022-10-26 01:22:45 +0300

    Bunny.

--- XP/xp01_janguru/janguru.py
+++ XP/xp01_janguru/janguru.py
@@ -1,6 +1,10 @@
+"""Janguru task."""
+
+
 def meet_me(pos1, jump_distance1, sleep1, pos2, jump_distance2, sleep2) -> int:
     """
     Calculate the meeting position of 2 jangurus.
+
     @:param pos1: position of first janguru
     @:param jump_distance1: jump distance of first janguru
     @:param sleep1: sleep time of first janguru
@@ -13,7 +17,7 @@ def meet_me(pos1, jump_distance1, sleep1, pos2, jump_distance2, sleep2) -> int:
     time = 0
     sleeping_1 = False
     sleeping_2 = False
-    while time < 100000000:
+    while time < 10000000:
         if time % sleep1 == 0:
             sleeping_1 = False
         if time % sleep2 == 0:
@@ -34,7 +38,7 @@ if __name__ == "__main__":
     print(meet_me(1, 2, 1, 2, 1, 1))  # => 3
     print(meet_me(10, 7, 7, 5, 8, 6))  # => 45
     print(meet_me(100, 7, 4, 300, 8, 6))  # => 940
-    print(meet_me(1, 7, 1, 15, 5, 1))  #  => 50
+    print(meet_me(1, 7, 1, 15, 5, 1))  # => 50
     print(meet_me(1, 1, 1, 1, 1, 1))  # => 2
     print(meet_me(1, 1, 1000, 10, 1, 9000))  # => 12
     print(meet_me(1, 1, 1000, 10, 1, 9001))  # => 11
