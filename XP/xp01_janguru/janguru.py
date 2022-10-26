1fb4f1dc8bb14184eac52bb474567d5f39f47f7c
anviks <anviks@ttu.ee>
2022-10-26 12:45:47 +0300

    Bunny.

--- XP/xp01_janguru/janguru.py
+++ XP/xp01_janguru/janguru.py
@@ -17,18 +17,29 @@ def meet_me(pos1, jump_distance1, sleep1, pos2, jump_distance2, sleep2) -> int:
     that_is_enough = 0
     time1 = 0
     time2 = 0
-    while that_is_enough < 2500000:
-        if time1 <= time2:
-            pos1 += jump_distance1
-            time1 += sleep1
-        if pos1 == pos2 and time1 - sleep1 != time2:
-            return pos1
-        if time2 <= time1:
-            pos2 += jump_distance2
-            time2 += sleep2
+    while that_is_enough < 2000000:
         that_is_enough += 1
-        if pos1 == pos2:
-            return pos1
+        if -1000000 < pos1 - pos2 < 1000000:
+            if time1 <= time2:
+                pos1 += jump_distance1
+                time1 += sleep1
+            if pos1 == pos2 and time1 - sleep1 != time2:
+                return pos1
+            if time2 <= time1:
+                pos2 += jump_distance2
+                time2 += sleep2
+            if pos1 == pos2:
+                return pos1
+        elif pos1 - pos2 > 1000000:
+            difference = pos1 - pos2
+            skip_time = difference // (sleep2 * sleep1)
+            pos1 += jump_distance1 * (skip_time * sleep2)
+            pos2 += jump_distance2 * (skip_time * sleep1)
+        elif pos2 - pos1 > 1000000:
+            difference = pos2 - pos1
+            skip_time = difference // (sleep2 * sleep1)
+            pos1 += jump_distance1 * (skip_time * sleep2)
+            pos2 += jump_distance2 * (skip_time * sleep1)
     return -1
 
 
@@ -49,3 +60,4 @@ if __name__ == "__main__":
     print(meet_me(250, 35, 26, 284, 149, 111))  # => 880
     print(meet_me(92, 151, 200, 28, 109, 144))  # => 6132
     print(meet_me(201, 87, 209, 118, 107, 257))  # => 5682
+    print(meet_me(10000000, 1, 5, 1, 1, 2))
