d5fe5b678791de04c8ac60b55fd41cc431abc16b
anviks <anviks@ttu.ee>
2022-10-29 20:14:23 +0300

    Style fixed.

--- EX/ex01_hello/cashier.py
+++ EX/ex01_hello/cashier.py
@@ -1,5 +1,5 @@
 """Calculates the change needed to give."""
 amount = int(input("Enter a sum: "))
 coins = amount // 50 + amount % 50 // 20 + amount % 50 % 20 // 10 + amount % 50 % 20 % 10 // 5 +\
-        amount % 50 % 20 % 10 % 5
+    amount % 50 % 20 % 10 % 5
 print(f"Amount of coins needed: {coins}")
