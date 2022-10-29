29671d2a3cc473e3341905510fb9d1959b241cb7
anviks <anviks@ttu.ee>
2022-10-29 13:07:42 +0300

    Kt.

---
+++ MX/mx_simple_oop/student.py
@@ -0,0 +1,15 @@
+"""Simple OOP task."""
+
+
+class Student:
+    """Student class."""
+    def __init__(self, name, finished=False):
+        """Constructor."""
+        self.name = name
+        self.finished = finished
+
+
+if __name__ == '__main__':
+    student = Student("John")
+    print(student.name)  # John
+    print(student.finished)  # False
