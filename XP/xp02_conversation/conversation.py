a511ec0ceca0ee46c63e1ec2375f9acf962a322a
anviks <anviks@ttu.ee>
2022-11-04 13:15:13 +0200

    Let's see what happens.

--- XP/xp02_conversation/conversation.py
+++ XP/xp02_conversation/conversation.py
@@ -1,4 +1,4 @@
-"""Converstaion."""
+"""Conversation."""
 import re
 import math
 
@@ -26,7 +26,7 @@ class Student:
         f"Possible answers are {sorted_list_of_possible_answers_in_growing_sequence)}." if there are multiple possibilities
         f"The number I needed to guess was {final_answer}." if the result is certain
         """
-        return f"Possible answers are {sorted(self.possible_answers)}."
+        return f"Possible answers are {sorted(self.possible_answers)}"
 
     def intersect_possible_answers(self, update: list):
         """
@@ -509,7 +509,7 @@ def find_catalan_numbers(biggest_number: int):
 
 
 def catalan(num):
-    """Finds a catalan number."""
+    """Find catalan number."""
     if num <= 1:
         return 1
     result = 0
@@ -518,9 +518,9 @@ def catalan(num):
     return result
 
 
-# regex_a = r'((?:- )?\d+)x2'
-# regex_b = r'((?:- )?\d+)?x(?!2)'
-# regex_c = r'(?<!x)((?:- )?\d+)(?!x)'
+regex_a = r'((?:- )?\d+)x2'
+regex_b = r'((?:- )?\d+)?x(?!2)'
+regex_c = r'(?<!x)((?:- )?\d+)(?!x)'
 
 if __name__ == '__main__':
 
