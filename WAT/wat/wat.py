4d6c0d6777f7da476512837e9646fe129713c522
anviks <anviks@ttu.ee>
2022-11-25 12:16:17 +0200

    horror

---
+++ WAT/wat/wat.py
@@ -0,0 +1,34 @@
+"""Wat???"""
+
+
+def function_a():
+    pass
+
+
+def function_b():
+    pass
+
+
+def function_c():
+    pass
+
+
+def function_d():
+    pass
+
+
+def function_e():
+    pass
+
+
+def function_f():
+    pass
+
+
+def function_g():
+    pass
+
+
+def function_h():
+    pass
+
