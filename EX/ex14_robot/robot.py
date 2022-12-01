0db4b9e444aeef5a9c031287402858652af0e527
anviks <anviks@ttu.ee>
2022-12-01 23:36:49 +0200

    bot

--- EX/ex14_robot/robot.py
+++ EX/ex14_robot/robot.py
@@ -1,3 +1,6 @@
+"""Something about robots."""
+
+
 from FollowerBot import FollowerBot
 
 
@@ -5,7 +8,7 @@ def test_run(robot: FollowerBot):
     """
     Make the robot move, doesnt matter how much, just as long as it has moved from the starting position.
 
-    :param FollowerBot robot: instance of the robot that you need to make move
+    :param FollowerBot robot: instance of the robot that you need to make move.
     """
     robot.set_wheels_speed(30)
     robot.sleep(2)
