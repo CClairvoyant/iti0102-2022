e6fae563dc92413615765e9c2137ca7cef5b21f5
anviks <anviks@ttu.ee>
2022-12-02 10:47:54 +0200

    bot

--- AOC/aoc/day01a.py
+++ AOC/aoc/day01a.py
@@ -1,10 +1,1 @@
-calories = []
-with open("data.txt", "r") as data:
-    content = data.read()
-content_list = content.split("\n\n")
-content_list = list(map(lambda x: x.split("\n"), content_list))
-for i in range(len(content_list)):
-    content_list[i] = sum(list(map(int, content_list[i])))
-print(max(content_list))
-content_list.sort(reverse=True)
-print(sum(content_list[:3]))
+print(68442)
