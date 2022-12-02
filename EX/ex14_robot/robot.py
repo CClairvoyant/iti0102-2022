d9b221c8fd7d19314981dadfe9eb676ffce53e98
anviks <anviks@ttu.ee>
2022-12-02 19:37:12 +0200

    bot

--- EX/ex14_robot/robot.py
+++ EX/ex14_robot/robot.py
@@ -41,19 +41,19 @@ def follow_the_line(robot: FollowerBot):
     """
     while robot.get_second_line_sensor_from_left() != 0 and robot.get_second_line_sensor_from_right() != 0:
         robot.set_wheels_speed(20)
-        robot.sleep(0.05)
+        robot.sleep(0.01)
     while robot.get_right_line_sensor() == 0 and robot.get_left_line_sensor() == 0 or \
             robot.get_second_line_sensor_from_right() == 0 and robot.get_second_line_sensor_from_left() == 0:
         robot.set_wheels_speed(20)
-        robot.sleep(0.05)
+        robot.sleep(0.01)
         while robot.get_third_line_sensor_from_left() == 0:
             robot.set_wheels_speed(0)
             robot.set_right_wheel_speed(20)
-            robot.sleep(0.05)
+            robot.sleep(0.01)
         while robot.get_third_line_sensor_from_right() == 0:
             robot.set_wheels_speed(0)
             robot.set_left_wheel_speed(20)
-            robot.sleep(0.05)
+            robot.sleep(0.01)
     robot.done()
 
 
