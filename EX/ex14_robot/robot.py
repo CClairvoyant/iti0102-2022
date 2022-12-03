a9ace3b4dd5db7a528ead75854f147926ca2928c
anviks <anviks@ttu.ee>
2022-12-03 13:51:37 +0200

    bot

--- EX/ex14_robot/robot.py
+++ EX/ex14_robot/robot.py
@@ -88,7 +88,7 @@ def the_true_follower(robot: FollowerBot):
         if 300 < robot.get_right_line_sensor() < 700:
             robot.set_right_wheel_speed(100)
             robot.set_left_wheel_speed(-100)
-            robot.sleep(0.288007)
+            robot.sleep(0.288008)
     robot.done()
 
 
