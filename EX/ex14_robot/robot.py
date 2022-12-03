5784e282656e8d7fcd15a0d44ebdea32f0cd8bba
anviks <anviks@ttu.ee>
2022-12-03 12:00:20 +0200

    bot

--- EX/ex14_robot/robot.py
+++ EX/ex14_robot/robot.py
@@ -69,13 +69,17 @@ def the_true_follower(robot: FollowerBot):
         robot.set_wheels_speed(70)
         robot.sleep(0.01)
     while robot.get_position() != start:
-        while robot.get_third_line_sensor_from_left() == 0 and robot.get_left_line_sensor() != 0 or \
-                robot.get_third_line_sensor_from_left() == 0 and robot.get_right_line_sensor() != 0:
+        while robot.get_third_line_sensor_from_right() != robot.get_third_line_sensor_from_left() == 0 and \
+                robot.get_left_line_sensor() != 0 or \
+                robot.get_third_line_sensor_from_right() != robot.get_third_line_sensor_from_left() == 0 and \
+                robot.get_right_line_sensor() != 0:
             robot.set_left_wheel_speed(-70)
             robot.set_right_wheel_speed(70)
             robot.sleep(0.01)
-        while robot.get_third_line_sensor_from_right() == 0 and robot.get_left_line_sensor() != 0 or \
-                robot.get_third_line_sensor_from_right() == 0 and robot.get_right_line_sensor() != 0:
+        while robot.get_third_line_sensor_from_left() != robot.get_third_line_sensor_from_right() == 0 and \
+                robot.get_left_line_sensor() != 0 or \
+                robot.get_third_line_sensor_from_left() != robot.get_third_line_sensor_from_right() == 0 and \
+                robot.get_right_line_sensor() != 0:
             robot.set_right_wheel_speed(-70)
             robot.set_left_wheel_speed(70)
             robot.sleep(0.01)
