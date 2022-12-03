b3a430737e3933eed4df423f1f2aa7f31194ece5
anviks <anviks@ttu.ee>
2022-12-03 09:57:37 +0200

    christmas :)

--- EX/ex14_robot/robot.py
+++ EX/ex14_robot/robot.py
@@ -48,11 +48,11 @@ def follow_the_line(robot: FollowerBot):
         robot.sleep(0.01)
         while robot.get_third_line_sensor_from_left() == 0:
             robot.set_wheels_speed(0)
-            robot.set_right_wheel_speed(7)
+            robot.set_right_wheel_speed(30)
             robot.sleep(0.001)
         while robot.get_third_line_sensor_from_right() == 0:
             robot.set_wheels_speed(0)
-            robot.set_left_wheel_speed(7)
+            robot.set_left_wheel_speed(30)
             robot.sleep(0.001)
     robot.done()
 
