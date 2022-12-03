70b84d4012d9e0c0fe4fd50d433f86d00f96df39
anviks <anviks@ttu.ee>
2022-12-03 12:17:24 +0200

    bot

--- EX/ex14_robot/robot.py
+++ EX/ex14_robot/robot.py
@@ -73,15 +73,15 @@ def the_true_follower(robot: FollowerBot):
                 robot.get_left_line_sensor() != 0 or \
                 robot.get_third_line_sensor_from_right() != robot.get_third_line_sensor_from_left() == 0 and \
                 robot.get_right_line_sensor() != 0:
-            robot.set_left_wheel_speed(-70)
-            robot.set_right_wheel_speed(70)
+            robot.set_left_wheel_speed(-75)
+            robot.set_right_wheel_speed(75)
             robot.sleep(0.01)
         while robot.get_third_line_sensor_from_left() != robot.get_third_line_sensor_from_right() == 0 and \
                 robot.get_left_line_sensor() != 0 or \
                 robot.get_third_line_sensor_from_left() != robot.get_third_line_sensor_from_right() == 0 and \
                 robot.get_right_line_sensor() != 0:
-            robot.set_right_wheel_speed(-75)
-            robot.set_left_wheel_speed(75)
+            robot.set_right_wheel_speed(-70)
+            robot.set_left_wheel_speed(70)
             robot.sleep(0.01)
         robot.set_wheels_speed(75)
         robot.sleep(0.01)
