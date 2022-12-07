e8e8e3371a8e728c262b840a785d79755eac9496
anviks <anviks@ttu.ee>
2022-12-07 14:23:40 +0200

    Pyramid comprehension.

---
+++ MX/mx_pyramid/pyramid.py
@@ -0,0 +1,115 @@
+"""Program that creates beautiful pyramids."""
+
+
+def make_pyramid(base: int, char: str) -> list:
+    """
+    Construct a pyramid with given base.
+
+    Pyramid should consist of given chars, all empty spaces in the pyramid list are ' '. Pyramid height depends on base length. Lowest floor consists of base-number chars.
+    Every floor has 2 chars less than the floor lower to it.
+    make_pyramid(3, "A") ->
+    [
+        [' ', 'A', ' '],
+        ['A', 'A', 'A']
+    ]
+    make_pyramid(6, 'a') ->
+    [
+        [' ', ' ', 'a', 'a', ' ', ' '],
+        [' ', 'a', 'a', 'a', 'a', ' '],
+        ['a', 'a', 'a', 'a', 'a', 'a']
+    ]
+    :param base: int
+    :param char: str
+    :return: list
+    """
+
+    pyramid = [
+        list(" " * ((base - i) // 2) + char * i + " " * ((base - i) // 2))
+        for i in range(1 if base % 2 else 2, base + 1, 2)
+    ]
+
+    return pyramid
+
+
+def join_pyramids(pyramid_a: list, pyramid_b: list) -> list:
+    """
+    Join together two pyramid lists.
+
+    Get 2 pyramid lists as inputs. Join them together horizontally. If the the pyramid heights are not equal, add empty lines on the top until they are equal.
+    join_pyramids(make_pyramid(3, "A"), make_pyramid(6, 'a')) ->
+    [
+        [' ', ' ', ' ', ' ', ' ', 'a', 'a', ' ', ' '],
+        [' ', 'A', ' ', ' ', 'a', 'a', 'a', 'a', ' '],
+        ['A', 'A', 'A', 'a', 'a', 'a', 'a', 'a', 'a']
+    ]
+
+    :param pyramid_a: list
+    :param pyramid_b: list
+    :return: list
+    """
+    smaller_pyramid = pyramid_a if len(pyramid_a) < len(pyramid_b) else pyramid_b
+    larger_pyramid = pyramid_a if len(pyramid_a) > len(pyramid_b) else pyramid_b
+
+    smaller_pyramid = [list(" " * len(smaller_pyramid[0]))] * (
+                len(larger_pyramid) - len(smaller_pyramid)) + smaller_pyramid
+
+    merged_pyramid = [smaller_pyramid[i] + larger_pyramid[i] for i in range(len(smaller_pyramid))]
+
+    return merged_pyramid
+
+
+def to_string(pyramid: list) -> str:
+    """
+    Return pyramid list as a single string.
+
+    Join pyramid list together into a string and return it.
+    to_string(make_pyramid(3, 'A')) ->
+    '''
+     A
+    AAA
+    '''
+
+    :param pyramid: list
+    :return: str
+    """
+    s = "\n".join("".join(row) for row in pyramid)
+    return s
+
+
+if __name__ == '__main__':
+    pyramid_a = make_pyramid(3, "A")
+    print(pyramid_a)  # ->
+    """
+    [
+        [' ', 'A', ' '],
+        ['A', 'A', 'A']
+    ]
+    """
+
+    pyramid_b = make_pyramid(6, 'a')
+    print(pyramid_b)  # ->
+    """
+    [
+        [' ', ' ', 'a', 'a', ' ', ' '],
+        [' ', 'a', 'a', 'a', 'a', ' '],
+        ['a', 'a', 'a', 'a', 'a', 'a']
+    ]
+    """
+
+    joined = join_pyramids(pyramid_a, pyramid_b)
+    print(joined)  # ->
+    """
+    [
+        [' ', ' ', ' ', ' ', ' ', 'a', 'a', ' ', ' '],
+        [' ', 'A', ' ', ' ', 'a', 'a', 'a', 'a', ' '],
+        ['A', 'A', 'A', 'a', 'a', 'a', 'a', 'a', 'a']
+    ]
+    """
+
+    pyramid_string = to_string(joined)
+    print(pyramid_string)  # ->
+    """
+         aa
+     A  aaaa
+    AAAaaaaaa
+    """
