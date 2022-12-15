ca172d950ca73ed258efd8237e6d4956a0275526
anviks <anviks@ttu.ee>
2022-12-15 22:00:38 +0200

    idk

---
+++ XP/xp_auction/player.py
@@ -0,0 +1,18 @@
+"""MX Auction."""
+from auction import Auction  # copy the files auction.py and bots.py!
+
+
+class PlayerBot:
+    """Bot algorithm."""
+
+    def __init__(self):
+        """Initialize the class, put all the variables that you want to save here."""
+
+    def play(self, money: int, opponent_money: int, last_bid: int) -> int:
+        """Play a round in the auction."""
+        return min(15, money)  # example algorithm that bids up to 15 every round
+
+
+if __name__ == '__main__':
+    # play all the example bots against each other
+    Auction(main_player=PlayerBot).simulate(auctions=99, rounds=99)
