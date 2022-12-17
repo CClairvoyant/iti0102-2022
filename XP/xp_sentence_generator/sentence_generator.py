e81308e3667782d00c3340e93c12ac77f7b6588b
anviks <anviks@ttu.ee>
2022-12-17 22:43:17 +0200

    Unlucky.

--- XP/xp_sentence_generator/sentence_generator.py
+++ XP/xp_sentence_generator/sentence_generator.py
@@ -9,8 +9,8 @@ class SentenceGenerator:
         self.rules = rules_string.strip().split("\n")
         self.rule_dict = {}
         self.indexes = {}
-        
-        # Add rules to rule_dict (values as list) and to indexes if they are plain words, only to rule_dict 
+
+        # Add rules to rule_dict (values as list) and to indexes if they are plain words, only to rule_dict
         # (values as strings) if they contain previously defined rules.
         for rule in self.rules:
             if "|" in rule:
@@ -26,14 +26,14 @@ class SentenceGenerator:
     def sentence_generator(self, syntax: str):
         """Build the sentence."""
         syntaxes = syntax.split()
-        
+
         while True:
             result = ""
-            
+
             # Loop through the given syntaxes and use the saved indexes to build the sentence.
             for syn in syntaxes:
                 if isinstance(self.rule_dict[syn], list):
-                    result += self.rule_dict[syn][self.indexes[syn] % len(self.rule_dict[syn])]
+                    result += self.rule_dict[syn][self.indexes[syn] % len(self.rule_dict[syn])] + " "
                     self.indexes[syn] += 1
                 else:
                     for word in self.rule_dict[syn].split():
@@ -42,8 +42,8 @@ class SentenceGenerator:
                         else:
                             result += self.rule_dict[word][self.indexes[word] % len(self.rule_dict[word])] + " "
                             self.indexes[word] += 1
-                            
-            yield result
+
+            yield result[:-1]
 
 
 if __name__ == '__main__':
@@ -70,4 +70,6 @@ if __name__ == '__main__':
     gg = g.sentence_generator("beautifulsentence noun")
     print(next(gg))
     print(next(gg))
+    print(next(gg))
+    gg = g.sentence_generator("twosentences")
     print(next(gg))
