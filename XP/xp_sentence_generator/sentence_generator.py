6ac18209921c260bf51ccb99bc527cb29b9ac48d
anviks <anviks@ttu.ee>
2022-12-18 20:37:34 +0200

    Unlucky.

--- XP/xp_sentence_generator/sentence_generator.py
+++ XP/xp_sentence_generator/sentence_generator.py
@@ -2,52 +2,36 @@
 
 
 class SentenceGenerator:
-    """The main class."""
+    """A class for generating sentences based on a set of rules."""
 
     def __init__(self, rules_string: str):
-        """Initialize the class and save all the rules and indexes for each rule."""
-        self.rules = rules_string.split("\n")
+        """Initialize the class and save all the rules and indexes for each rule.
+
+        :param rules_string: A string containing the rules for generating sentences.
+        """
         self.rule_dict = {}
         self.indexes = {}
+        self.construct_dictionary(rules_string)
 
-        # Add rules to rule_dict (values as list) and to indexes if they are plain words, only to rule_dict
-        # (values as strings) if they contain previously defined rules.
-        for rule in self.rules:
+    def construct_dictionary(self, rules_string: str):
+        """Process the rules string and populate the rule_dict dictionary.
+
+        :param rules_string: A string containing the rules for generating sentences.
+        """
+        for rule in rules_string.split("\n"):
             if "|" in rule:
                 key = rule.split(" =")[0]
                 self.indexes[key] = 0
                 self.rule_dict[key] = []
                 for value in rule.split("= ")[1].split(" | "):
                     if key != value:
-                        if any(["." in value, "," in value, "!" in value, "?" in value]):
-                            punctuation_indexes = []
-                            if "." in value:
-                                punctuation_indexes.append(value.index("."))
-                            if "," in value:
-                                punctuation_indexes.append(value.index(","))
-                            if "!" in value:
-                                punctuation_indexes.append(value.index("!"))
-                            if "?" in value:
-                                punctuation_indexes.append(value.index("?"))
-                            index = min(punctuation_indexes)
-                            value = value[:index] + " temp " + value[index:]
+                        value = self.check_punctuation(value)
                         self.rule_dict[key].append(value)
             else:
                 # test_lines_with_some_rules
                 if "=" in rule:
                     key, value = rule.split(" = ")
-                    if any(["." in value, "," in value, "!" in value, "?" in value]):
-                        punctuation_indexes = []
-                        if "." in value:
-                            punctuation_indexes.append(value.index("."))
-                        if "," in value:
-                            punctuation_indexes.append(value.index(","))
-                        if "!" in value:
-                            punctuation_indexes.append(value.index("!"))
-                        if "?" in value:
-                            punctuation_indexes.append(value.index("?"))
-                        index = min(punctuation_indexes)
-                        value = value[:index] + " temp " + value[index:]
+                    value = self.check_punctuation(value)
                     if key not in self.rule_dict:
                         self.rule_dict[key] = value
                     elif isinstance(self.rule_dict[key], list):
@@ -58,9 +42,35 @@ class SentenceGenerator:
                     if self.rule_dict[key] == key:
                         self.rule_dict[key] = "???"
 
-    def sentence_generator(self, syntax: str):
-        """Build the sentence."""
+    @staticmethod
+    def check_punctuation(value: str):
+        """Check if a value contains punctuation and add a placeholder if it does.
 
+        :param value: The value to check for punctuation.
+
+        :return: The value with a placeholder added if it contains punctuation, or the original value if it does not.
+        """
+        if any(["." in value, "," in value, "!" in value, "?" in value]):
+            punctuation_indexes = []
+            if "." in value:
+                punctuation_indexes.append(value.index("."))
+            if "," in value:
+                punctuation_indexes.append(value.index(","))
+            if "!" in value:
+                punctuation_indexes.append(value.index("!"))
+            if "?" in value:
+                punctuation_indexes.append(value.index("?"))
+            index = min(punctuation_indexes)
+            value = value[:index] + " temp " + value[index:]
+        return value
+
+    def sentence_generator(self, syntax: str):
+        """Generate a sentence based on the provided syntax.
+
+        :param syntax: The syntax to use for generating the sentence.
+
+        :return: The generated sentence.
+        """
         syntaxes = syntax.split(" ")
 
         while True:
@@ -80,15 +90,20 @@ class SentenceGenerator:
             for word in result.split(" "):
                 if word in self.rule_dict:
                     result = result[:result.index(word)] + next(self.sentence_generator(word)) + \
-                             result[result.index(word) + len(word):]
+                            result[result.index(word) + len(word):]
 
             if " temp " in result:
                 result = result.replace(" temp ", "")
 
             yield result[:-1]
 
-    def get_word(self, word):
-        """Use a rule to get a word."""
+    def get_word(self, word: str) -> str | list:
+        """Use a rule to get a word.
+
+        :param word: The rule to use for getting the word.
+
+        :return: The generated word.
+        """
         if word in self.indexes:
             self.indexes[word] += 1
             return self.rule_dict[word][(self.indexes[word] - 1) % len(self.rule_dict[word])] + " "
@@ -117,5 +132,3 @@ if __name__ == '__main__':
     print(next(gg))
     print(next(gg))
     print(next(gg))
-
-
