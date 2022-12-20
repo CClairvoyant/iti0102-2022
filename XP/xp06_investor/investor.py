78c167aca44dea472c69fe36e5131c18b41bdb54
anviks <anviks@ttu.ee>
2022-12-20 12:51:44 +0200

    First push.

--- XP/xp06_investor/investor.py
+++ XP/xp06_investor/investor.py
@@ -72,11 +72,16 @@ def exchange_money(exchange_rates: dict) -> list:
     while len(drops) > len(optimal_merge(drops)):
         drops = optimal_merge(drops)
 
-    drops = remove_bad_drops(drops)
-
-    for drop in drops:
+    for drop in remove_bad_drops(drops.copy()):
         dates.append(drop["start_date"])
         dates.append(drop["end_date"])
+
+    if not dates:
+        highest = max(drops, key=lambda x: x["start_value"])
+        lowest = min(drops[drops.index(highest):], key=lambda x: x["end_value"])
+        if highest["start_value"] * 99 / 100 / lowest["end_value"] * 99 / 100 > 1:
+            dates.append(highest["start_date"])
+            dates.append(lowest["end_date"])
 
     return dates
 
@@ -111,6 +116,15 @@ def optimal_merge(drops: list[dict]) -> list[dict]:
     return drops
 
 
+# def compare_stacks_of_drops(drops: list[dict], count: int) -> list[dict]:
+#     """Compare more drops with each other at once."""
+#     for i in range(len(drops) + 1 - count):
+#         sum1 = 0
+#         sum2 = 0
+#         for x in range(count):
+#             sum1 +=
+
+
 def remove_bad_drops(drops: list[dict]) -> list[dict]:
     """Remove points, where investing would lose you money, due to bank's cut."""
     for i in range(len(drops)):
@@ -125,5 +139,29 @@ def remove_bad_drops(drops: list[dict]) -> list[dict]:
 
 if __name__ == '__main__':
     result = get_currency_rates_from_file("article-report.csv")
-    print(result)
-    print(len(exchange_money(result[1])))
+    test_data = {
+        '31-10-2016': 1.0946,
+        '28-10-2016': 1.0922,
+        '27-10-2016': 1.0927,
+        '26-10-2016': 1.0925,
+        '25-10-2016': 1.0872,
+        '24-10-2016': 1.0891,
+        '21-10-2016': 1.0886,
+        '20-10-2016': 1.0980,
+        '19-10-2016': 1.0979,
+        '18-10-2016': 1.0993,
+        '17-10-2016': 1.0994,
+        '14-10-2016': 1.1002,
+        '13-10-2016': 1.1038,
+        '12-10-2016': 1.1020,
+        '11-10-2016': 1.1079,
+        '10-10-2016': 1.1160,
+        '07-10-2016': 1.1140,
+        '06-10-2016': 1.1185,
+        '05-10-2016': 1.1211,
+        '04-10-2016': 1.1161,
+        '03-10-2016': 1.1236
+    }
+    print(exchange_money(test_data))
+    # 03 10 2016
+    # 25 10 2016
